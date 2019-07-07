52b52070f6eedffa8f62946b212b361fafde1669
Hidayatullah Agung <hidayatullahap@gmail.com>
2019-07-07 15:39:23 +0700

    add error handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,23 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { HttpErrorFilter } from './errors/http-error.filter';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './errors/logging.interceptor';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(), 
-    UsersModule, PostsModule
-  ],
+  imports: [TypeOrmModule.forRoot(), UsersModule, PostsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
