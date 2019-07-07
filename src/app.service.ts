207972a41a1d63dd2e373453915c4e4831520b43
Hidayatullah Agung <hidayatullahap@gmail.com>
2019-07-07 17:35:05 +0700

    change hello route

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  async getHello(): Promise<any> {
+    return new Promise(resolve => {
+      resolve({ message: "pong" })
+    });
   }
 }
